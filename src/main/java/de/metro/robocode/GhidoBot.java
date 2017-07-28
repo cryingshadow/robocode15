8d5c3bfaf9ae4fec62eddb00b8afd6b1eafcc236
Mihaaai <ghidome@gmail.com>
2017-07-28 16:02:39 +0300

    Changed name

--- src/main/java/de/metro/robocode/GhidoBot.java
+++ src/main/java/de/metro/robocode/GhidoBot.java
@@ -1,29 +1,88 @@
+
 package de.metro.robocode;
 
-import robocode.*;
+
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
+
 
 public class GhidoBot extends Robot {
 
-    @Override
+    boolean peek; // Don't turn if there's a robot there
+    double moveAmount; // How much to move
+
+    /**
+     * run: Move around the walls
+     */
+
+    private void colorRobot()
+    {
+        // Set colors
+        setBodyColor(Color.black);
+        setGunColor(Color.black);
+        setRadarColor(Color.orange);
+        setBulletColor(Color.cyan);
+        setScanColor(Color.cyan);
+
+    }
+
     public void run() {
 
-        double radius = 100.0;
-        double angle = 90.0;
+        colorRobot();
+
+        // Initialize moveAmount to the maximum possible for this battlefield.
+        moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+        // Initialize peek to false
+        peek = false;
+
+        // turnLeft to face a wall.
+        // getHeading() % 90 means the remainder of
+        // getHeading() divided by 90.
+        turnLeft(getHeading() % 90);
+        ahead(moveAmount);
+        // Turn the gun to turn right 90 degrees.
+        peek = true;
+        turnGunRight(90);
+        turnRight(90);
 
         while (true) {
-            ahead(radius);
-            turnLeft(angle);
-            turnGunLeft(angle);
-            fireBullet(getEnergy());
+            // Look before we turn when ahead() completes.
+            peek = true;
+            // Move up the wall
+            ahead(moveAmount);
+            // Don't look now
+            peek = false;
+            // Turn to the next wall
+            turnRight(90);
         }
     }
 
+    /**
+     * onHitRobot:  Move away a bit.
+     */
+    public void onHitRobot(HitRobotEvent e) {
+        // If he's in front of us, set back up a bit.
+        if (e.getBearing() > -90 && e.getBearing() < 90) {
+            back(100);
+        } // else he's in back of us, so set ahead a bit.
+        else {
+            ahead(100);
+        }
+    }
+
+    /**
+     * onScannedRobot:  Fire!
+     */
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        fire(2);
+        // Note that scan is called automatically when the robot is moving.
+        // By calling it manually here, we make sure we generate another scan event if there's a robot on the next
+        // wall, so that we do not start moving up it until it's gone.
+        if (peek) {
+            scan();
+        }
     }
-
-    public void onHitByBullet(HitByBulletEvent e) {
-        turnLeft(90 - e.getBearing());
-    }
-
 }
